b33d597d5b1feceef12479ef1ef23b0b9a9ef1e5
Pascal Neumann <openmia2022@gmail.com>
2022-12-28 11:45:55 +0100

    test

--- src/openMia.php
+++ src/openMia.php
@@ -3,18 +3,34 @@ namespace openMia\openMia;
 
 use openMia\openMia\FileSystem\Path;
 
-use openMia\composer\composer;
+//use openMia\composer\composer;
 
 class openMia {
     private $rootDir = "";
     
     public function __construct() {
         /* use mergePath for correct DirectorySeparator */
-        $this->rootDir = Path::mergePath("",__DIR__."../../../../..");
+        $this->rootDir = trim(Path::mergePath("",__DIR__."../../../../.."),DIRECTORY_SEPARATOR);
 
-        
-        echo $this->rootDir;
+
+        echo $this->registerApps($this->rootDir);
         $composer = new composer();
+    }
+
+    public function registerApps($rootDir) {
+        $regPath = $rootDir.DIRECTORY_SEPARATOR."registry";
+        if(!is_dir($regPath)) {
+            mkdir($regPath);
+            return false;
+        }
+        $appFile = $regPath.DIRECTORY_SEPARATOR."apps.json";
+        if(!is_file($appFile)) {
+            return false;
+        }
+        if($fileData = file_get_contents($appFile)) {
+            $appData = json_decode($fileData);
+
+        } else return false;
     }
 
 }
